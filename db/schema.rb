cfeaff8c77ec07174cccc3746100fd78194d435c
Richard Young <richarddfyoung@gmail.com>
2022-11-22 13:53:45 -0500

    species model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_223109) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_22_170543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "invasive?"
+    t.boolean "edible?"
+    t.string "usage"
+    t.string "photo_url"
+    t.string "season"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
