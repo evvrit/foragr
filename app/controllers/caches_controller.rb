d189393d7f5b476307cc869d94d0b54dd1e049c6
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-22 13:26:31 -0500

    added links to head of html and defined markers in caches index action

--- app/controllers/caches_controller.rb
+++ app/controllers/caches_controller.rb
@@ -2,5 +2,12 @@ class CachesController < ApplicationController
   def index
     @caches = policy_scope(Cache)
     authorize @caches
+
+    @markers = @caches.map do |cache|
+      {
+        lat: cache.latitude,
+        lng: cache.longitude
+      }
+    end
   end
 end
