536c4c5dd0835d554efe5809bf0e3d1c2797e099
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-22 15:28:30 -0500

    added teh heroku repo to the gh repo by accident and deleted it lol

--- app/controllers/caches_controller.rb
+++ app/controllers/caches_controller.rb
@@ -6,7 +6,8 @@ class CachesController < ApplicationController
     @markers = @caches.map do |cache|
       {
         lat: cache.latitude,
-        lng: cache.longitude
+        lng: cache.longitude,
+        cache_info: render_to_string(partial: "cache_info", locals: {cache: cache})
       }
     end
   end
