b8636d402de01f211977f68a6a15cf2fb9026703
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-24 15:05:08 -0500

    js action is connected and working

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -2,10 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-favorite"
 export default class extends Controller {
-
   static targets = ["link"]
 
   connect() {
     console.log(this.linkTarget);
+  }
+
+  toggleIconFill(event) {
+    // console.log(event);
+    this.linkTarget.classList.toggle("btn");
   }
 }
