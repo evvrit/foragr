617902d07d76cad90686f01ad46999764ac2e465
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-24 12:19:20 -0500

    active storage changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_165513) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
