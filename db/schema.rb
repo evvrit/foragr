7dc4b35de1f76f7ff029ca3356d37eae0d8447b8
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-23 19:37:26 -0500

    added title column to log model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_003223) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_003614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_003223) do
     t.bigint "cache_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["cache_id"], name: "index_logs_on_cache_id"
     t.index ["user_id"], name: "index_logs_on_user_id"
   end
