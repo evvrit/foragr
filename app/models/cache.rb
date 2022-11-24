141e15749323752e56e6bd3e6b769345534b33c6
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-23 19:43:49 -0500

    added validations to both cache and log models for length of title + crash tested

--- app/models/cache.rb
+++ app/models/cache.rb
@@ -9,4 +9,5 @@ class Cache < ApplicationRecord
   validates :longitude, :latitude, presence: true
   validates :description, length: { minimum: 30 }
   validates :found_on, presence: true
+  validates :title, length: { in: 4..36 }
 end
