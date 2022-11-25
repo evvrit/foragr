ef5249a18e911639d3edea0ddded1a29369a6001
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-25 16:54:02 -0500

    favorited caches now dynamically remove from collection

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -2,21 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-favorite"
 export default class extends Controller {
-  static targets = ["link", "card"]
+  static targets = ["link", "card", "cacheCard"]
 
   connect() {
-    // console.log(this.cardTarget);
+    console.log();
   }
 
-  toggleIconFill(event) {
+  toggleIconFill() {
     // console.log(this.linkTarget);
     // this.linkTarget.className == 'fa-solid fa-heart-circle-minus fa-2xl' ? 'fa-solid fa-heart-circle-plus fa-2xl' : 'fa-solid fa-heart-circle-minus fa-2xl';
     ['fa-heart-circle-plus', 'fa-heart-circle-minus'].map(v=> this.linkTarget.classList.toggle(v) )
-    this.showCard();
+    if (this.hasCardTarget) {
+      this.showCard();
+    }
+
+    if (this.hasCacheCardTarget) {
+      console.log(this.cacheCardTarget);
+      // console.log("hello");
+      this.removeCacheCard();
+    }
   }
 
   showCard() {
     // console.log(event);
     this.cardTarget.classList.toggle('d-none')
+  }
+
+  removeCacheCard() {
+    // console.log(this.cacheCardTarget);
+    this.cacheCardTarget.classList.add('d-none')
   }
 }
