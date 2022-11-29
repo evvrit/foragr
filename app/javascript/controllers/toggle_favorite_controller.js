283f77d3426321256e9961a70f8c3cd639d86d38
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-29 15:11:49 -0500

    toggle favorite icon works on species show page, it helps if you connect the controller

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -2,17 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-favorite"
 export default class extends Controller {
-  static targets = ["link", "card", "cacheCard", "icon"]
+  static targets = ["link", "card", "cacheCard"]
 
   connect() {
-    console.log(this.iconTarget);
+    console.log(this.linkTarget);
   }
 
   toggleIconFill() {
     console.log(this.linkTarget);
     // this.linkTarget.className == 'fa-solid fa-heart-circle-minus fa-2xl' ? 'fa-solid fa-heart-circle-plus fa-2xl' : 'fa-solid fa-heart-circle-minus fa-2xl';
     ['fa-heart-circle-plus', 'fa-heart-circle-minus'].map(v=> this.linkTarget.classList.toggle(v) )
-    // ['fa-heart-circle-plus', 'fa-heart-circle-minus'].map(v=> this.iconTarget.classList.toggle(v) )
 
     if (this.hasCardTarget) {
       this.showCard();
