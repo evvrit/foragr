b9e96f4167b5eda28c5f4a1f7c43b1a586f92774
Elyse Turpin <turpin.elyse@gmail.com>
2022-11-30 23:03:24 -0500

    interrelated caches and species seeds so the photos makes sense

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@ Species.destroy_all
 User.destroy_all
 puts "DB is clean."
 
-# Seeding users
-puts "making 6 users..."
+# puts "=== making 6 users ==="
+# # puts "making 6 users..."
 
-User.create(email: "a@a.a", password: "secret", username: "anna")
-User.create(email: "b@b.b", password: "secret", username: "bob")
+# User.create(email: "a@a.a", password: "secret", username: "anna")
+# User.create(email: "b@b.b", password: "secret", username: "bob")
 
-4.times do User.create!(
-  email: Faker::Internet.email,
-  password: "secret",
-  username: Faker::Twitter.screen_name
-)
-end
+# 4.times do User.create!(
+#   email: Faker::Internet.email,
+#   password: "secret",
+#   username: Faker::Twitter.screen_name
+# )
+# end
 
-puts "6 users made."
+# puts ">>> 6 users made."
 
 # # Photos from assets for seeding
 # PHOTOS = [
@@ -37,18 +37,18 @@ puts "6 users made."
 #   "walnut.jpg"
 # ]
 
-# GENERIC_TITLES = [
-#   "I absolutely love finding treasures growing from the ground",
-#   "what a wonderful adventure I had that day",
-#   "could not have asked for a better haul",
-#   "couldn't find what I was looking for; found somethiing even better!",
-#   "these were delicious",
-#   "looks like a brain lol",
-#   "is this past its prime?",
-#   "epic haul from an honest day's worth of foraging",
-#   "this stuff was growing in my backyard!",
-#   "we brought some to the neighbours :)"
-# ]
+GENERIC_TITLES = [
+  "I absolutely love finding treasures growing from the ground",
+  "what a wonderful adventure I had that day",
+  "could not have asked for a better haul",
+  "couldn't find what I was looking for; found somethiing even better!",
+  "these were delicious",
+  "looks like a brain lol",
+  "is this past its prime?",
+  "epic haul from an honest day's worth of foraging",
+  "this stuff was growing in my backyard!",
+  "we brought some to the neighbours :)"
+]
 
 
 # # Seeding species
@@ -180,11 +180,34 @@ puts "6 users made."
 # puts "12 additional favorites seeded."
 
 # ===
+
 # Making very specific seeds for demo
 
 # Users will stay the same
 
+puts "        "
+puts "making 6 users..."
+# puts "making 6 users..."
+
+User.create(email: "a@a.a", password: "secret", username: "anna")
+User.create(email: "b@b.b", password: "secret", username: "bob")
+
+4.times do User.create!(
+  email: Faker::Internet.email,
+  password: "secret",
+  username: Faker::Twitter.screen_name
+)
+end
+
+puts ">>> 6 users made."
+
 # Seeding species, on which all things depend
+
+puts "         "
+puts "seeding species..."
+
+# the specific species and order in which they appear in species_names is
+# integral to the matching of cache and species photos.
 
 species_names = [
   "Brown Birch Bolete", "Chanterelles",
@@ -199,7 +222,7 @@ species_names = [
 
 def create_species(identifier)
   @identifier = species_scraper(identifier)
-  @species = Species.create!(
+  species = Species.create!(
     name: @identifier[:name],
     overview: @identifier[:overview],
     features: @identifier[:features],
@@ -213,17 +236,129 @@ def create_species(identifier)
     photos: @identifier[:photos]
   )
   puts "made #{identifier} a species"
-  return @species
+  return species
 end
 
-@species_instances = species_names.map do |identifier|
+species_instances = species_names.map do |identifier|
   create_species(identifier)
 end
 
+# puts ">>>first @species photo: #{@species_instances.first.photos.first}"
+puts ">>> #{species_instances.length} species created."
 
-puts "first @species name: #{@species_instances.first.name}"
+puts "          "
+puts "seeding caches"
+
 # Seeding caches, with appropriate photos
 
-# Seeding logs, with appropriate photos
+PHOTOS = [
+  "brownbirchbolete.jpg",
+  "chanterelle.jpg",
+  "chickenofthewoods.jpg",
+  "morel.jpg",
+  "beggarticks.jpg",
+  "bractedorache.jpeg",
+  "catnip.jpg",
+  "chamomile.jpeg",
+  "commonreed.jpg",
+  "commonyarrow.jpeg",
+  "juniper.jpg",
+  "easternredbud.jpg",
+  "canyongrape.jpg",
+  "elmoyster.jpg"
+]
+
+def create_caches(times)
+  counter = 0
+  caches = []
+  times.times do
+    cache = Cache.create!(
+      user: User.all.sample,
+      longitude: [Faker::Number.within(range: -73.83053621933163..-73.56830061735444),
+                  Faker::Number.within(range: -73.56708451801268..-73.36224401489754)].sample,
+      latitude: Faker::Number.within(range: 45.4372900644492..45.6280127081368),
+      description: Faker::Lorem.paragraphs(number: 1), # this should not be lorem ipsum
+      found_on: Faker::Date.between(from: 5.days.ago, to: Date.today),
+      seed_photo: PHOTOS[counter],
+      title: GENERIC_TITLES.sample
+    )
+    counter += 1
+    caches << cache
+  end
+  return caches
+end
+
+cache_instances = create_caches(3)
+
+# puts ">>>first @cache photo: #{cache_instances.first.seed_photo}"
+puts ">>> #{cache_instances.length} caches created."
 
 # Seeding CacheSpecies
+
+puts "         "
+puts "Assigning Species to all caches and creating CacheSpecies"
+
+def create_cachespecy
+  counter = 0
+  Cache.all.each do |cache|
+    CacheSpecy.create(cache: cache, species: Species.all[counter])
+    counter += 1
+  end
+end
+
+puts "Caches now all have a corresponding species assigned"
+
+# Seeding logs, with appropriate photos
+# unfortunately these are the same pics as caches, will diversify if we have time
+
+puts "         "
+puts "making 100 logs..."
+
+100.times do
+  log = Log.new(
+    content: Faker::Lorem.paragraphs(number: 1), # this should not be lorem
+    created_on: Faker::Date.between(from: 5.days.ago, to: Date.today),
+    user_id: User.all.sample.id,
+    cache_id: Cache.all.sample.id,
+    title: GENERIC_TITLES.sample
+  )
+  log.seed_photo = Cache.find_by(id: log.cache_id).seed_photo
+  log.save!
+end
+
+puts "100 logs created."
+
+# Seeding favorites
+
+puts "        "
+puts "favoriting caches and species..."
+
+2.times { User.first.favorite(Species.all.sample) }
+3.times { User.first.favorite(Cache.all.sample) }
+
+puts "a@a.a has favorited #{User.first.favorites_by_type('Species').length} species"
+puts "and #{User.first.favorites_by_type('Cache').length} caches."
+
+2.times { User.second.favorite(Species.all.sample) }
+3.times { User.second.favorite(Cache.all.sample) }
+
+puts "b@b.b has favorited #{User.second.favorites_by_type('Species').length} species"
+puts "and #{User.second.favorites_by_type('Cache').length} caches."
+
+puts "distributing 12 additional favorites randomly..."
+
+12.times { User.all.sample.favorite(Species.all.sample) }
+
+puts "12 additional favorites seeded."
+
+puts "             "
+puts "
+                        .-'~~~-.
+                      .'o  oOOOo`.
+                      :~~~-.oOo   o`.
+  GO TEAM foragr !    `. \ ~-.  oOOo.
+                        `.; / ~.  OO:
+                        .'  ;-- `.o.'
+                      ,'  ; ~~--'~
+                      ;  ;
+_______\|/__________\\;_\\//___\|/________"
