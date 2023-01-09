68aa69dbde79ba9cbbab95d0b83b032b48f31db6
evvrit <turpin.elyse@gmail.com>
2023-01-09 14:13:56 -0500

    removed ability to drop pin on form page, introduced a new event to add an obstacle to dropping a pin from main map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,17 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import { Controller } from "@hotwired/stimulus";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 
 // Connects to data-controller="map"
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
-  }
+    markers: Array,
+  };
 
   connect() {
-
     this.link = document.getElementById("new-cache-link");
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
 
     mapboxgl.workerCount = 12;
     mapboxgl.prewarm();
@@ -23,16 +22,20 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10?optimize=true",
       center: [-73.60488, 45.53257],
       zoom: 10,
-    })
+    });
 
-    this.map.on('load', () => {
-      const mapContainerEl = document.getElementById('map');
-      mapContainerEl.style.visibility = 'visible';
+    this.map.on("load", () => {
+      const mapContainerEl = document.getElementById("map");
+      mapContainerEl.style.visibility = "visible";
     });
 
     if (this.markersValue.length > 1) {
-      this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl }))
+      this.map.addControl(
+        new MapboxGeocoder({
+          accessToken: mapboxgl.accessToken,
+          mapboxgl: mapboxgl,
+        })
+      );
     }
 
     this.map.doubleClickZoom.disable();
@@ -46,73 +49,81 @@ export default class extends Controller {
       this.static = document.getElementById("sm-static");
       this.lat = this.markersValue[0].lng; // just trust me
       this.lng = this.markersValue[0].lat;
-
     } else {
       this.static = document.getElementById("static");
-      this.lat = -73.60488
-      this.lng = 45.53257
+      this.lat = -73.60488;
+      this.lng = 45.53257;
     }
     const width = this.static.offsetWidth;
     const height = this.static.offsetHeight;
-    this.static.style.backgroundImage = `url(https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${this.lat},${this.lng},10/${width}x${height}?access_token=${this.apiKeyValue})`
+    this.static.style.backgroundImage = `url(https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/${this.lat},${this.lng},10/${width}x${height}?access_token=${this.apiKeyValue})`;
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-        const popup = new mapboxgl.Popup({className: "popup-container", closeOnClick: true, focusAfterOpen: false})
+      const popup = new mapboxgl.Popup({
+        className: "popup-container",
+        closeOnClick: true,
+        focusAfterOpen: false,
+      })
         .setHTML(marker.cache_info)
-        .setMaxWidth("250px")
-        let one_marker = new mapboxgl.Marker({color: '#35635B'})
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(this.map)
-        if (this.markersValue.length > 1) {
-          one_marker.setPopup(popup, {focusAfterOpen: true}).addTo(this.map);
-        }
-    })
+        .setMaxWidth("250px");
+      let one_marker = new mapboxgl.Marker({ color: "#35635B" })
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(this.map);
+      if (this.markersValue.length > 1) {
+        one_marker.setPopup(popup, { focusAfterOpen: true }).addTo(this.map);
+      }
+    });
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
     if (this.markersValue.length === 1) {
-      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
     }
   }
 
   #checkClickLocation() {
-    this.map.on('click', (e) => {
-      const offset = 0.01 // we use it to build a selection area around each marker
-      const clickLat = e.lngLat.lat
-      const clickLng = e.lngLat.lng
-      let userClickedMarker = false
-      this.markersValue.forEach(marker => {
+    this.map.on("dblclick", (e) => {
+      const offset = 0.01; // we use it to build a selection area around each marker
+      const clickLat = e.lngLat.lat;
+      const clickLng = e.lngLat.lng;
+      let userClickedMarker = false;
+      this.markersValue.forEach((marker) => {
         // this actually checks the longitude. error in seed do not touch !!!
-        const clickedWithinLat = clickLat > (marker.lat) && clickLat < (marker.lat + (2 * offset))
+        const clickedWithinLat =
+          clickLat > marker.lat && clickLat < marker.lat + 2 * offset;
         // actually checks latitude
-        const clickedWithinLng = clickLng > (marker.lng - offset) && clickLng < (marker.lng + offset)
+        const clickedWithinLng =
+          clickLng > marker.lng - offset && clickLng < marker.lng + offset;
         if (clickedWithinLat && clickedWithinLng) {
-          userClickedMarker = true
-          return
+          userClickedMarker = true;
+          return;
         }
-      })
-      if (!userClickedMarker) {
+      });
+      if (!userClickedMarker && this.markersValue.length > 1) {
+        console.log(this.markersValue.length);
         this.#dropPin(e);
         this.#sendCoordsToForm(e);
       }
-    })
+    });
   }
 
   #dropPin(e) {
-    new mapboxgl.Marker({color: '#957009'})
-    .setLngLat(e.lngLat)
-    .addTo(this.map);
-  };
+    new mapboxgl.Marker({ color: "#957009" })
+      .setLngLat(e.lngLat)
+      .addTo(this.map);
+  }
 
   #sendCoordsToForm(e) {
-    this.link.href = `${this.link.href}?lat=${e.lngLat.lat}&lng=${e.lngLat.lng}`
+    this.link.href = `${this.link.href}?lat=${e.lngLat.lat}&lng=${e.lngLat.lng}`;
     new mapboxgl.Marker()
-    .setLngLat([ e.lngLat.lat, e.lngLat.lng ])
-    .addTo(this.map)
+      .setLngLat([e.lngLat.lat, e.lngLat.lng])
+      .addTo(this.map);
     this.link.click();
   }
 }
